9558c8d2e6e5f8b3c1f826795ca787b275205a61
Chitkul Lakshya <chitkullakshya@gmail.com>
2026-06-01 05:00:00 +0530

    Fix mobile responsive layout in LandingPage

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,3 +1,6 @@
 
 // Update related to: Migrate database schema for candidates
 // Implementation details added on 2026-06-01T00:00:00
+
+// Update related to: Fix mobile responsive layout in LandingPage
+// Implementation details added on 2026-06-01T05:00:00
