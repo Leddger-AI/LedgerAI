fbd5135963f767918afe41aa23cabde0b2bc66da
Chitkul Lakshya <chitkullakshya@gmail.com>
2026-06-02 11:00:00 +0530

    Fix date parsing across timezones

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,3 +1,6 @@
 
 // Update related to: Fix CORS issue on production server
 // Implementation details added on 2026-06-02T08:00:00
+
+// Update related to: Fix date parsing across timezones
+// Implementation details added on 2026-06-02T11:00:00
