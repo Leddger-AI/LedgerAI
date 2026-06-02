d5f32be1ac873cbdb6af398ece4a3042328ab032
Chitkul Lakshya <chitkullakshya@gmail.com>
2026-06-02 09:00:00 +0530

    Enhance security of JWT signing

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -4,3 +4,6 @@
 
 // Update related to: Fix date parsing across timezones
 // Implementation details added on 2026-06-02T11:00:00
+
+// Update related to: Enhance security of JWT signing
+// Implementation details added on 2026-06-02T09:00:00
