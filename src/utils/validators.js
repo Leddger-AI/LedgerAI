1d6700b4014d6462368c42ab44af070267c4c365
Chitkul Lakshya <chitkullakshya@gmail.com>
2026-06-06 07:00:00 +0530

    Improve error messages in UI

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -13,3 +13,6 @@
 
 // Update related to: Add rate limiting to backend APIs
 // Implementation details added on 2026-06-13T12:00:00
+
+// Update related to: Improve error messages in UI
+// Implementation details added on 2026-06-06T07:00:00
